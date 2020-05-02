5ba8609fc00b24ed09b6675ba79c05dcf3108b5e
Emmanuel <Emmaccen@users.noreply.github.com>
2020-05-02 02:19:07 +0100

    Added insta post contents

--- src/components/baseSectionContainer/Instagram.js
+++ src/components/baseSectionContainer/Instagram.js
@@ -5,9 +5,48 @@ class Instagram extends React.Component {
 
     render () {
         return (
-            <div>
+            <div className='instaPostsWrapper'>
                 <div className='instaHeader'>
-                    <h1>Latest From <span className='highlight'>Instagram</span></h1>
+                    <div>
+                        <h1 className='heading2'>Latest From <span className='highlight'>Instagram</span></h1>
+                        <p className='descriptionText2'>One company owner asks another: “Tell me, Bill, how come your employees are always on time 
+                            in the mornings?” Bill replies: “Easy. 30 employees and 20 parking spaces.”
+                        </p>
+                    </div>
+                </div>
+
+                <div className='instaPostsContainer'>
+
+                    <div className='grid-1'>
+                        <div style={{backgroundImage : `url(${'instaImages/1.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                        </div>
+                        <div className='split'>
+                            <div style={{backgroundImage : `url(${'instaImages/2.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                            </div>
+                            <div style={{backgroundImage : `url(${'instaImages/3.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='grid-2'>
+
+                        <div className='split'>
+                            <div style={{backgroundImage : `url(${'instaImages/4.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                            </div>
+                            <div style={{backgroundImage : `url(${'instaImages/5.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                            </div>
+                        </div>
+
+                        <div style={{backgroundImage : `url(${'instaImages/6.jpg'}`}} className='backgroundFix instaBg'>
+                            
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         )
