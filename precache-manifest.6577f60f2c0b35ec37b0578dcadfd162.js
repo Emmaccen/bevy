02c770270d31c601bf5a87ffef35f846888b1319
Emmanuel <Emmaccen@users.noreply.github.com>
2020-05-02 14:16:53 +0100

    Updates

---
+++ precache-manifest.6577f60f2c0b35ec37b0578dcadfd162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3cf28bb98c78dca593ce3ba73170f1",
+    "url": "/bevy/index.html"
+  },
+  {
+    "revision": "f77db0ff27ffd6c8c107",
+    "url": "/bevy/static/css/main.7ab03518.chunk.css"
+  },
+  {
+    "revision": "94faf5bcc7bf7049b942",
+    "url": "/bevy/static/js/2.1ee332b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bevy/static/js/2.1ee332b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77db0ff27ffd6c8c107",
+    "url": "/bevy/static/js/main.71ea3de6.chunk.js"
+  },
+  {
+    "revision": "7e77f8f1608faa17ce20",
+    "url": "/bevy/static/js/runtime-main.d5667673.js"
+  }
+]);
