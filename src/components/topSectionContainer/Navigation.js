6264161fcd0149c9f1d1221027d86795ecc97400
Emmanuel <Emmaccen@users.noreply.github.com>
2020-05-04 20:35:53 +0100

    worked on the menu responsiveness

--- src/components/topSectionContainer/Navigation.js
+++ src/components/topSectionContainer/Navigation.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 class Navigation extends React.Component {
 
+  // scrollToView(element){
+  //   document.getElementById(element).scrollIntoView({
+  //   behavior: 'smooth'
+  // });
+
+  closeNav () {
+    document.querySelector('.navigation').style.display = 'none'
+    // document.getElementById('navIcon').style.
+  }
+  openNav () {
+    let nav = document.querySelector('.navigation')
+    nav.style.display = 'block'
+    nav.style.WebkitAnimation = "fadeIn 1.3s"; // Code for Chrome, Safari and Opera
+    nav.style.animation = "fadeIn 1.3s";     // Standard syntax
+  }
+
   render(){
+
     return (
       <div className="navContainer">
         <nav className="container">
@@ -14,21 +31,25 @@ class Navigation extends React.Component {
             </div>
           </div>
 
-          <div>
-            <ul>
+          <div id='anim' className='navigation'>
+
+          <span style={{display : 'none'}} class="closeBtn" onClick={()=> this.closeNav()}>&times;</span>
+
+            <ul className='menuLinks'>
               <li><a href="#">Home</a></li>
               <li><a href="#">Menu</a></li>
               <li><a href="#">Services</a></li>
               <li><a href="#">Blogs</a></li>
               <li><a href="#">About</a></li>
               <li><a href="#">Contact</a></li>
-              <li><a href="#">Support</a></li>
+              {/* <li><a href="#">Support</a></li> */}
             </ul>
           </div>
 
           <div className='cart centered'>
-            <span className="icon icon-cart"></span>
-            <span>3</span>
+                <span title='Open Cart' className="icon icon-cart"></span>
+                <span id='cartNumber'>3</span>
+                <span onClick={()=> this.openNav()} title='Open Menu' className='menuIcon icon icon-menu'></span>
           </div>
 
         </nav>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+window.addEventListener('resize', console.log('clicked ...'))
 ReactDOM.render(
   <React.StrictMode>
     <App />
