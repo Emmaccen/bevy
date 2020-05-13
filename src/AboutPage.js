0613107bc9ca6b2d9e4797be910e0f3a10e4d667
Emmanuel <Emmaccen@users.noreply.github.com>
2020-05-13 01:33:10 +0100

    Created contents for single pages
    Fixed up the layout contents for the seperate pages on the website. Pretty cool huh ? :smile:

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import Navigation from './components/topSectionContainer/Navigation'
 import PageMaps from './components/PageMaps'
+import QuickContact from './components/topSectionContainer/QuickContact'
+import Welcome from './components/topSectionContainer/Welcome'
+import Meet from './components/baseSectionContainer/Meet'
+import Footer from './components/baseSectionContainer/Footer'
 
 class AboutPage extends React.Component {
 
 
     render () {
         return (
-            <div className='backgroundFix aboutPage page'>
-                <Navigation />
+            <div className='aboutPageWrapper'>
+                <div className='backgroundFix aboutPage page'>
+                    <Navigation />
                     <div className='pageMapContainer'>
                         <PageMaps 
                         icon = 'iconFlat flaticon-pancake'
                         pageText = 'About Us'
                         currentPage = 'About'/>
                     </div>  
+                </div>
+
+                <QuickContact />
+                <Welcome />
+                <Meet />
+                <Footer />
             </div>
         )
     }
